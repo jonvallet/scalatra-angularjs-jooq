256071226de2b2e735a88b3f05377fbc972201fd
Jon Vallet <j.vallet@validus-ivc.co.uk>
2015-11-17 17:20:32 +0000

    Add get call

--- web/src/main/webapp/js/controllers.js
+++ web/src/main/webapp/js/controllers.js
@@ -1,12 +1,8 @@
 var todoApp = angular.module('todoApp', []);
 
-todoApp.controller('TodoCtrl', function ($scope) {
-  $scope.todos = [
-    {"id":1,"name":"name","description":"description","done":false},
-    {"id":2,"name":"name2","description":"description2","done":true},
-    {"id":3,"name":"name3","description":"description3","done":true},
-    {"id":4,"name":"name3","description":"description3","done":false}
-  ];
+todoApp.controller('TodoCtrl', function ($scope,$http) {
+
+  $scope.todos = []
   $scope.newTodo = {}
   $scope.new = function(todo) {
     var newItem = {"id":$scope.todos.length+1,
@@ -15,4 +11,9 @@ todoApp.controller('TodoCtrl', function ($scope) {
                    "done":false}
     $scope.todos.push(newItem)
   }
+
+  $http.get('/api/todo').then(function successCallback(response) {
+    $scope.todos = response.data
+  })
+
 });
