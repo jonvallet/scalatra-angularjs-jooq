65f2d3dbb71c11cab079e34c56437e8e679b8a9f
Jon Vallet <j.vallet@validus-ivc.co.uk>
2015-12-10 10:53:37 +0000

    Revert "Add restaurant controller" This reverts commit ef13351300126d7dfd2ad952b940ff81e5ebd3ad.

--- web/src/main/scala/ScalatraBootstrap.scala
+++ web/src/main/scala/ScalatraBootstrap.scala
@@ -1,6 +1,6 @@
 import com.jonvallet.scalatra.angular._
 import com.jonvallet.scalatra.angular.database.{DatabaseContext, Database}
-import com.jonvallet.scalatra.angular.rest.{RestauranResource, TodoResource}
+import com.jonvallet.scalatra.angular.rest.TodoResource
 import com.mchange.v2.c3p0.ComboPooledDataSource
 import org.jooq.SQLDialect
 import org.scalatra._
@@ -13,7 +13,6 @@ class ScalatraBootstrap extends LifeCycle {
     Database.startup
     context.mount(new MyScalatraServlet, "/info/*")
     context.mount(new TodoResource(new DatabaseContext(cpds.getConnection, SQLDialect.H2)), "/api/todo/*")
-    context.mount(new RestauranResource(new DatabaseContext(cpds.getConnection, SQLDialect.H2)), "/api/todo/*")
   }
   override def destroy(context: ServletContext) {
     cpds.close()
